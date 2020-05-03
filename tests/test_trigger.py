066855b04d25ae5a3d4eca3d3ee069af5f61f90c
Torben <mail@torben.co>
2020-05-03 19:16:21 +0200

    more trigger tests

--- tests/test_trigger.py
+++ tests/test_trigger.py
@@ -27,7 +27,7 @@ def test_context(f):
 class SensorManagerTest(TestCase):
 
 	@test_context
-	def test_greater(self, sensor_manager, event_manager):
+	def test_evaluate_greater(self, sensor_manager, event_manager):
 		id = make_sensor_with_data(sensor_manager).id
 		vars = {
 			"t": {"id": id, "channel": "time", "row": -1},
@@ -41,3 +41,28 @@ class SensorManagerTest(TestCase):
 		assert parser.evaluate("uptime < 0", vars, sensor_manager) == False
 		assert parser.evaluate("uptime > 0 and t < 1e20 and 'cat' in 'bobcats' and not 'cat' in 'bobcAts'",
 		                       vars, sensor_manager)
+
+
+	@test_context
+	def test_save_load(self, sensor_manager, event_manager):
+		id = make_sensor_with_data(sensor_manager).id
+		vars = {
+			"t": {"id": id, "channel": "time", "row": -1},
+			"uptime": {"id": id, "channel": "seconds", "row": -1},
+		}
+
+		# make a trigger
+		t = trigger.Trigger(name="test", variables=vars, expression="t < 4", sensor_manager=sensor_manager)
+
+		# serialize and deserialize
+		new = trigger.Trigger.from_json(t.to_json())
+		assert t.to_json() == new.to_json()
+
+		assert t.evaluate(sensor_manager) == False
+		assert t.check(sensor_manager)["state"] == False
+
+		assert new.evaluate(sensor_manager) == False
+
+		# change the expression
+		new.expression = "t > 4"
+		assert new.evaluate(sensor_manager)
