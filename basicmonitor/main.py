f7a1613497d1d41c412ddee6401393cdca7441c5
Torben <mail@torben.co>
2020-06-09 22:43:44 +0200

    rudimentary serve from with prefix

--- basicmonitor/main.py
+++ basicmonitor/main.py
@@ -3,18 +3,22 @@ from flask_restful import Api
 #from flask_compress import Compress
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="build")
 CORS(app)
 #Compress(app)
 api = Api(app)
 
 
-@app.route('/')
+prefix = "/subdir2"
+
+@app.route(f'{prefix}')
+@app.route(f'{prefix}/')
+@app.route(f'{prefix}/index.html')
 def index():
-	return react_app("index.html")
+	return render_template("index.html", prefix=prefix)
 
 
-@app.route('/<path:path>')
+@app.route(f'{prefix}/<path:path>')
 def react_app(path):
 	return send_from_directory('build', path)
 
@@ -27,23 +31,23 @@ from basicmonitor.api.query import Query
 from basicmonitor.api.evaluate import Evaluate
 
 # list sensors and add new ones
-api.add_resource(SensorApi, '/sensors', '/sensors/')
-api.add_resource(SensorDetailApi, '/sensors/<string:item_id>')
-api.add_resource(SensorUpdateApi, '/sensors/<string:item_id>/update')
+api.add_resource(SensorApi, f'{prefix}/sensors', f'{prefix}/sensors/')
+api.add_resource(SensorDetailApi, f'{prefix}/sensors/<string:item_id>')
+api.add_resource(SensorUpdateApi, f'{prefix}/sensors/<string:item_id>/update')
 # triggers
-api.add_resource(TriggerApi, '/triggers', '/triggers/')
-api.add_resource(TriggerDetailApi, '/triggers/<string:item_id>')
-api.add_resource(TriggerUpdateApi, '/triggers/<string:item_id>/update')
+api.add_resource(TriggerApi, f'{prefix}/triggers', f'{prefix}/triggers/')
+api.add_resource(TriggerDetailApi, f'{prefix}/triggers/<string:item_id>')
+api.add_resource(TriggerUpdateApi, f'{prefix}/triggers/<string:item_id>/update')
 # actions
-api.add_resource(ActionApi, '/actions', '/actions/')
-api.add_resource(ActionDetailApi, '/actions/<string:item_id>')
-api.add_resource(ActionNotifyApi, '/actions/<string:item_id>/update')
+api.add_resource(ActionApi, f'{prefix}/actions', f'{prefix}/actions/')
+api.add_resource(ActionDetailApi, f'{prefix}/actions/<string:item_id>')
+api.add_resource(ActionNotifyApi, f'{prefix}/actions/<string:item_id>/update')
 # events
-api.add_resource(EventsApi, '/events')
+api.add_resource(EventsApi, f'{prefix}/events')
 # database queries
-api.add_resource(Query, '/query/<string:sensor_id>')
+api.add_resource(Query, f'{prefix}/query/<string:sensor_id>')
 # evaluate an expression, just as triggers do
-api.add_resource(Evaluate, '/evaluate')
+api.add_resource(Evaluate, f'{prefix}/evaluate')
 
 
 if __name__ == '__main__':
