65e6214eb4922900b2a61e9acefaf642e2043137
Torben <mail@torben.co>
2020-06-11 15:22:02 +0200

    command line deployment works :)

--- basicmonitor/__main__.py
+++ basicmonitor/__main__.py
@@ -1,3 +1,24 @@
+# this file is run using when using "python -m basicmonitor"
+
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Run baiscmonitor')
+parser.add_argument(
+	'-prefix',
+	type=str,
+	help='Prefix to run the webapp and api from. The prefix will be applied to every route.',
+	default="",
+)
+parser.add_argument(
+	'-port', '-p',
+	type=str,
+	help='Port on which the flask server will listen',
+	default=5000,
+)
+args = parser.parse_args()
+
+
 from basicmonitor import run
 
-run()
+run(prefix=args.prefix, port=args.port)
